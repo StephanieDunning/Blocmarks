657be7d756260c5760981adb9098dec4f56bf67f
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-06-22 19:50:09 -0700

    BookmarksCRUD: done

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should have_many(:bookmarks).dependent(:destroy) }
+  it { should validate_length_of(:title).is_at_least(3) }
 end
