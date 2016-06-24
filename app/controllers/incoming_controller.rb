03ab86988df2587bf777589b64516d23cf6aebf0
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-06-24 09:17:30 -0700

    IncomingEmails: done

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,12 +1,14 @@
 class IncomingController < ApplicationController
 
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!
 
   def create
-    # @user = User.find_by(email: params[:sender])
-    # @topic = Topic.find_by(title: params[:subject])
-    # @bookmark = @topic.bookmarks.build(url: @url)
-    # @bookmark.save
+    @user = User.find_by(email: params[:sender])
+    @topic = Topic.find_by(title: params[:subject])
+    @bookmark = @topic.bookmarks.build(url: params['body-plain'])
+    @bookmark.user = @user
+    @bookmark.save
     puts "INCOMING PARAMS HERE: #{params}"
     head 200
   end
