6450763b139c1a2e6d9a62130183c009077cfa87
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-07-17 11:52:35 -0500

    UserProfiles: updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @topic = current_user.topics
-    @liked_bookmarks = current_user.likes
-    @user_bookmarks = current_user.bookmarks
+    @grouped_liked_bookmarks = current_user.likes.group_by{|like| like.bookmark.topic}
+    @grouped_user_bookmarks = current_user.bookmarks.group_by(&:topic)
   end
 end
